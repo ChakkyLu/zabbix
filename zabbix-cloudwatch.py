aec8b8a49fb3fb10243a62efac73b7e325e0231a
rikuki <qlmzn@38f9d3cc2f3e.ant.amazon.com>
2019-12-12 17:31:06 +0800

    second commit

--- zabbix-cloudwatch.py
+++ zabbix-cloudwatch.py
@@ -19,7 +19,7 @@ def getCloudWatchData(r,s,d):
     # try:
     conn = boto3.client('cloudwatch', region_name=r)
     aws_metric = {
-            "metric":"ApproximateNumberOfMessagesNotVisible",
+            "metric":"NumberOfMessagesSent",
             "statistics": ["Average"]
     }
 
@@ -27,7 +27,7 @@ def getCloudWatchData(r,s,d):
 
     metric_name = aws_metric['metric']
     statistics = aws_metric['statistics']
-    results = conn.get_metric_statistics(Namespace='AWS/',
+    results = conn.get_metric_statistics(Namespace='AWS/SQS',
                     MetricName=metric_name,
                     StartTime=start_time,
                     EndTime=end_time,
