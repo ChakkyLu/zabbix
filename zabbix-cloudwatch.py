fa96ba75607b5b51b3dd14a2db7d89ce01aeb4b3
rikuki <qlmzn@38f9d3cc2f3e.ant.amazon.com>
2019-12-13 15:18:48 +0800

    third commit

--- zabbix-cloudwatch.py
+++ zabbix-cloudwatch.py
@@ -18,39 +18,38 @@ def getCloudWatchData(r,s,m,st,clid,caid):
     start_time = end_time - datetime.timedelta(minutes=5)
     period = 5*60
 
-    try:
-        conn = boto3.client('cloudwatch', region_name=r)
-        aws_metric = {
-                "metric": m,
-                "statistics": st
-        }
+    # try:
+    conn = boto3.client('cloudwatch', region_name=r)
+    aws_metric = {
+            "metric": m,
+            "statistics": st
+    }
 
-        cloud_watch_data = ''
+    cloud_watch_data = ''
 
-        metric_name = aws_metric['metric']
-        statistics = aws_metric['statistics']
-        print(metric_name,statistics)
-        results = conn.get_metric_statistics(Namespace='AWS/ElastiCache',
-                        MetricName=metric_name,
-                        Dimensions=[
-                            {
-                                "Name": 'CacheClusterId',
-                                "Value": clid
-                            },
-                            {
-                                "Name": "CacheNodeId",
-                                "Value": caid
-                            }
-                        ],
-                        StartTime=start_time,
-                        EndTime=end_time,
-                        Period=period,
-                        Statistics=statistics)
-        datapoints = results['Datapoints'][-1][statistics]
-        print(datapoints)
+    metric_name = aws_metric['metric']
+    statistics = aws_metric['statistics']
+    results = conn.get_metric_statistics(Namespace='AWS/ElastiCache',
+                    MetricName=metric_name,
+                    Dimensions=[
+                        {
+                            "Name": 'CacheClusterId',
+                            "Value": clid
+                        },
+                        {
+                            "Name": "CacheNodeId",
+                            "Value": caid
+                        }
+                    ],
+                    StartTime=start_time,
+                    EndTime=end_time,
+                    Period=period,
+                    Statistics=statistics)
+    datapoints = results['Datapoints'][-1][statistics]
+    print(datapoints)
 
-    except:
-        print("Exception on collecting data")
+    # except:
+    #     print("Exception on collecting data")
 
 
 if __name__ == "__main__":
