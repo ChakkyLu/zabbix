9520a93e69ae123c83d6e81bbd62bf285e9eb9cd
rikuki <qlmzn@38f9d3cc2f3e.ant.amazon.com>
2019-12-13 15:07:54 +0800

    third commit

--- zabbix-cloudwatch.py
+++ zabbix-cloudwatch.py
@@ -1,12 +1,13 @@
 import boto3
 import datetime
 import pytz
+import sys
 
 
 utc_tz = pytz.timezone('UTC')
 
 
-def getCloudWatchData(r,s,d):
+def getCloudWatchData(r,s,m,st,clid,caid):
 
     r = 'cn-northwest-1'
     global period
@@ -15,45 +16,45 @@ def getCloudWatchData(r,s,d):
 
     end_time = datetime.datetime.now(tz=utc_tz)
     start_time = end_time - datetime.timedelta(minutes=5)
-    # print(end_time)
-    # end_time = datetime.datetime(2019,11,29,15,5,0)
-    # start_time = datetime.datetime(2019,11,29,15,0,0)
     period = 5*60
-    # end_time = end_time.strftime("%Y-%m-%d %H:%M:%S")
-    # start_time = start_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    # try:
-    conn = boto3.client('cloudwatch', region_name=r)
-    aws_metric = {
-            "metric":"EngineCPUUtilization",
-            "statistics": ["Average"]
-    }
+    try:
+        conn = boto3.client('cloudwatch', region_name=r)
+        aws_metric = {
+                "metric": m,
+                "statistics": st
+        }
 
-    cloud_watch_data = ''
+        cloud_watch_data = ''
 
-    metric_name = aws_metric['metric']
-    statistics = aws_metric['statistics']
-    results = conn.get_metric_statistics(Namespace='AWS/ElastiCache',
-                    MetricName=metric_name,
-                    Dimensions=[
-                        {
-                            "Name": "CacheNodeId",
-                            "Value": "0001"
-                        },
-                        {
-                            "Name": 'CacheClusterId',
-                            "Value": 'gitlab-redis-001'
-                        }
-                    ],
-                    StartTime=start_time,
-                    EndTime=end_time,
-                    Period=period,
-                    Statistics=statistics)
-    print(results)
+        metric_name = aws_metric['metric']
+        statistics = aws_metric['statistics']
+        results = conn.get_metric_statistics(Namespace='AWS/ElastiCache',
+                        MetricName=metric_name,
+                        Dimensions=[
+                            {
+                                "Name": 'CacheClusterId',
+                                "Value": clid
+                            },
+                            {
+                                "Name": "CacheNodeId",
+                                "Value": caid
+                            }
+                        ],
+                        StartTime=start_time,
+                        EndTime=end_time,
+                        Period=period,
+                        Statistics=statistics)
+        datapoints = results['Datapoints'][-1][statistics]
+        print(datapoints)
 
-    # except:
-    #     print("YES")
+    except:
+        print("Exception on collecting data")
 
 
 if __name__ == "__main__":
-    getCloudWatchData('cn-northwest-1', 'ElastiCache', '')
+    args = sys.argv
+    metric = args[1]
+    clid = args[2]
+    caid = '0001'
+    getCloudWatchData('cn-northwest-1', 'ElastiCache', metric, ['Average'], caid, clid)
